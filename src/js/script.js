ba6d1553712b72bf007823127620fa091f11056f
Nikita <karneges2012@gmail.com>
2020-01-03 18:09:29 +0200

    add tabs

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,41 @@ $(document).ready(function () {
             }
         ]
     });
+
+    let tab = document.querySelector('.catalog__tabs');
+    let tabs = document.querySelectorAll('.catalog__tab')
+    let catalogs = document.querySelectorAll('.catalog__content')
+
+    tab.addEventListener('click',(e)=>{
+        togleActive(e);
+    })
+    function togleActive (e){
+        tabs.forEach((item,i)=>{
+            if(e.target.parentElement == item){
+                item.classList.add('catalog__tab_active')
+                catalogs[i].classList.add('catalog__content_active')
+            }
+            else{
+                item.classList.remove('catalog__tab_active')
+                catalogs[i].classList.remove('catalog__content_active')
+            }
+            
+        })
+    }
+
+    let wrappers = document.querySelectorAll(`.catalog-item__wrapper`)
+    wrappers.forEach((item)=>{
+        item.addEventListener(`click`,(evt)=>{
+            evt.preventDefault()
+            if(evt.target = HTMLAnchorElement){
+                showList(evt)
+            }
+        })
+    })
+    function showList(evt){
+        evt.currentTarget.children[0].classList.toggle('catalog-item__content_active')
+        evt.currentTarget.children[1].classList.toggle('catalog-item__list_active')
+    }
 });
+
+
